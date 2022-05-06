f3d8519062c26d2f0a84abc2188315060bf64b93
suwanarat <104966043+suwanarat@users.noreply.github.com>
2022-05-06 13:20:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                 lat: 16.4639927,
+                 lng: 101.1459128,
             },
         },
     ];
